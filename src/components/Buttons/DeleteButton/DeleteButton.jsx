51acf87e09387e1eed8fd8a349ca6469cafbc943
StasSkv <stas000123@gmail.com>
2025-06-11 18:11:40 +0300

    update buttons

---
+++ src/components/Buttons/DeleteButton/DeleteButton.jsx
@@ -0,0 +1,24 @@
+import { MdDelete } from 'react-icons/md';
+import s from './DeleteButton.module.css';
+import { useDispatch } from 'react-redux';
+import { removeProductFromFavorites } from '../../../redux/favoritesSlice/favoritesSlice.js';
+import { toast } from 'react-toastify';
+
+export const DeleteButton = ({ id, onStartRemove }) => {
+  const dispatch = useDispatch();
+
+  const handleRemove = (e) => {
+    e.stopPropagation();
+    onStartRemove();
+    setTimeout(() => {
+      dispatch(removeProductFromFavorites(id));
+    }, 300);
+    toast.warning('Товар видалено з улюблених');
+  };
+
+  return (
+    <button className={s.deleteBtn} onClick={handleRemove}>
+      <MdDelete />
+    </button>
+  );
+};
